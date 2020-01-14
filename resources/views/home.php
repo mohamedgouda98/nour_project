9157e91dd95a77ed5556541d26687b5ba1585a7b
dev mohamed gouda <dev.mohamedgouda@gmail.com>
2020-01-14 13:29:05 +0200

    first form gouda

---
+++ resources/views/home.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+<!--
+    * Write Your Code Here .
+    * Your style page with name style.scss in scss folder.
+    * Don't for get install npm with "npm install"
+    * run npm with "npm run watch".
+@endsection
